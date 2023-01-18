6f6da215751b7ec77cc0e7dacb768911920737e7
P-BUS <pavlo.buslowski@gmail.com>
2023-01-19 00:38:45 +0200

    made model nullable

--- app/src/main/java/com/example/testtaskfore/data/network/PhotoApiService.kt
+++ app/src/main/java/com/example/testtaskfore/data/network/PhotoApiService.kt
@@ -5,15 +5,14 @@ import com.example.testtaskfore.data.model.UnsplashPhoto
 import com.example.testtaskfore.utils.Constants.ACCESS_KEY
 import com.example.testtaskfore.utils.Constants.ENDPOINT
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface PhotoApiService {
     @GET(ENDPOINT)
     suspend fun getPhotos(
         @Query("client_id") clientId: String = ACCESS_KEY,
-        @Query("page") pageNumber: Int = 1,
-        @Query("per_page") itemsPerPage: Int = 10,
+        @Query("page") pageNumber: Int = 5,
+        @Query("per_page") itemsPerPage: Int = 30,
         @Query("order_by") sort: String = "popular"
     ): List<UnsplashPhoto>
 
