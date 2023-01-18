82a4d4c646eb04de293d7c1d0757e7e25ae78f4e
P-BUS <pavlo.buslowski@gmail.com>
2023-01-18 19:05:00 +0200

    added collect of photos Flow in list fragment

--- app/src/main/java/com/example/testtaskfore/ui/ListFragment.kt
+++ app/src/main/java/com/example/testtaskfore/ui/ListFragment.kt
@@ -6,6 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
@@ -13,6 +16,8 @@ import com.example.testtaskfore.R
 import com.example.testtaskfore.databinding.ListFragmentBinding
 import com.example.testtaskfore.ui.adapters.PhotoListAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ListFragment : Fragment() {
@@ -40,7 +45,14 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_detailsFragment)
         }
         recyclerView.adapter = adapter
+
+        lifecycleScope.launch {
+            sharedViewModel.photos
+                .flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+                .distinctUntilChanged()
+                .collect {
+                    adapter.submitList(it)
+                }
+        }
     }
-
-
 }
