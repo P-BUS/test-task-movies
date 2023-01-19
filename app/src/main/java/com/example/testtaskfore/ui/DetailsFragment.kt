238aeae755aca0ac2e9290ca75a533215f553273
P-BUS <pavlo.buslowski@gmail.com>
2023-01-19 13:12:33 +0200

    detailed fragment added

--- app/src/main/java/com/example/testtaskfore/ui/DetailsFragment.kt
+++ app/src/main/java/com/example/testtaskfore/ui/DetailsFragment.kt
@@ -5,10 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import com.example.testtaskfore.R
 import com.example.testtaskfore.databinding.DetailsFragmentBinding
 
 class DetailsFragment : Fragment() {
+    private val sharedViewModel: PhotoViewModel by activityViewModels()
     private lateinit var binding: DetailsFragmentBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +21,18 @@ class DetailsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.details_fragment, container, false)
+    ): View {
+        binding = DetailsFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindPhoto()
+    }
+
+    private fun bindPhoto() {
+
     }
 
 }
--- app/src/main/java/com/example/testtaskfore/ui/MainActivity.kt
+++ app/src/main/java/com/example/testtaskfore/ui/MainActivity.kt
@@ -10,14 +10,14 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
+   /* private lateinit var navController: NavController*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment =
+       /* val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-            navController = navHostFragment.navController
+            navController = navHostFragment.navController*/
     }
 }
